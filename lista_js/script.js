de729f26f0e07548fd03e05bae2e81e9eff54a25
Gustavo Henrique Nascimento <gustavohn2017@gmail.com>
2024-04-18 15:51:17 +0000

    commiting changes

---
+++ lista_js/script.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('form-contato');
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const inputNome = document.getElementById('nome');
+    const inputEmail = document.getElementById('email');
+    const inputNumero = document.getElementById('numero');
+
+    let linha = '<tr>'
+    linha += `<td>${inputNome.value}</td>`
+    linha += `<td>${inputEmail.value}</td>`
+    linha += `<td>${inputNumero.value}</td>`
+    linha += '</tr>'
+
+    linhas += linha
+
+    const corpotabela = document.querySelector('tbody');
+    corpotabela.innerHTML = linhas;
+    inputNome.value = '';
+    inputEmail.value = '';
+    inputNumero.value = '';
+
+
+
+})
+    
